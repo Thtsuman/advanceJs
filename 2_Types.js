1d52ad77826621d08f41e09191b63351bc0da3d4
thtsuman <tht.suman@gmail.com>
2020-12-20 12:57:43 +0530

    changes

--- 2_Types.js
+++ 2_Types.js
@@ -21,5 +21,23 @@ diff btw null and undefined
 undefined is absence of defination
 null is absence of value
 
+as array is also object we can check weather an array is array not an object
+by =>  Array.isArray([])
+
+Passed by reference and passed by value
+
+primitive types are always immutable.. so whenever we want to change the 
+value of the variable it create another space in memory and assign that memory to
+that variable
+
+primitive types are always passed by value
+
+non primitve types are always passed by reference
+ie it will assign a pointer to that memory where the data is not making copy of that memory
+like passed by value method.
+
+type coercion
+language turning certain type to another type for comparison
+in js "==" do type coercion
 
 */
